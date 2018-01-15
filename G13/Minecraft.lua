52eea40121366137cc219b44679a559a0a933927
unknown <34061817+s10akir@users.noreply.github.com>
2018-01-16 07:53:22 +0900

    [add] G6押下時のみ後方確認ができるように

--- G13/Minecraft.lua
+++ G13/Minecraft.lua
@@ -1,3 +1,13 @@
 function OnEvent(event, arg)
     --OutputLogMessage("event = %s, arg = %s\n", event, arg);
+
+    -- G6で押している間だけ後方確認
+    if (arg == 6) then
+        if (event == "G_PRESSED") then
+            PressAndReleaseKey("F5")
+            PressAndReleaseKey("F5")
+        elseif (event == "G_RELEASED") then
+            PressAndReleaseKey("F5")
+        end
+    end
 end
