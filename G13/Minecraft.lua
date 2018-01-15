720052950d17188cb415ed146cf5fd26071408ea
unknown <34061817+s10akir@users.noreply.github.com>
2018-01-16 08:16:17 +0900

    [add] G21でホットバーをスイッチ

--- G13/Minecraft.lua
+++ G13/Minecraft.lua
@@ -1,3 +1,5 @@
+i = 0
+
 function OnEvent(event, arg)
     --OutputLogMessage("event = %s, arg = %s\n", event, arg);
 
@@ -8,6 +10,18 @@ function OnEvent(event, arg)
             PressAndReleaseKey("F5")
         elseif (event == "G_RELEASED") then
             PressAndReleaseKey("F5")
+        end
+    end
+
+    -- G21でホットバー1~4をスイッチ
+    -- [剣][ピッケル][シャベル][オノ] とかにしとくと便利
+    if (arg == 21) then
+        if (event == "G_PRESSED") then
+            PressAndReleaseKey(tostring(i))
+            i = i + 1
+            if (4 < i) then
+                i = 1
+            end
         end
     end
 end
